c0d8f0d79143a72cf04b50b3f4adc031a0621b7e
keyserjose044 <josemarquez044@gmail.com>
2025-11-23 17:26:08 -0600

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Dr. Rohitashva K. Singh — Concrete Materials & Structural Engineer";
-export const SITE_DESCRIPTION = "Tracking your life with Google Sheets!";
+export const SITE_DESCRIPTION = "Academic Profile";
