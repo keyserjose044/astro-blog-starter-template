e0f33e07527e347357d95cc3e1259206c383c37f
keyserjose044 <josemarquez044@gmail.com>
2025-11-23 17:25:45 -0600

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "LifeLoggerz";
+export const SITE_TITLE = "Dr. Rohitashva K. Singh — Concrete Materials & Structural Engineer";
 export const SITE_DESCRIPTION = "Tracking your life with Google Sheets!";
