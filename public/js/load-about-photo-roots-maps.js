d3774cdbafa71494022cd022e5953b98771541ae
keyserjose044 <josemarquez044@gmail.com>
2026-08-05 23:09:15 -0500

    Remove unused Family Roots loader

--- public/js/load-about-photo-roots-maps.js
+++
@@ -1,6 +0,0 @@
-(() => {
-  const script = document.createElement('script');
-  script.src = '/js/about-photo-roots-maps.js?v=20260805-family-roots-maps-v1';
-  script.defer = true;
-  document.head.appendChild(script);
-})();
